d775a0f14ace97ae4e2ca1819920c7370d7de1c5
thinkyside <eunlyuing@gmail.com>
2025-04-04 17:20:51 +0900

    [#4] CodeBaseUIView Protocol 구현

---
+++ poporazzi/Feature/1. MomentTitleInput/MomentTitleInputView.swift
@@ -0,0 +1,42 @@
+//
+//  MomentTitleInputView.swift
+//  poporazzi
+//
+//  Created by 김민준 on 4/4/25.
+//
+
+import UIKit
+import PinLayout
+import FlexLayout
+
+final class MomentTitleInputView: CodeBaseUIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+}
+
+// MARK: - Action
+
+extension MomentTitleInputView {
+    
+    enum Action {
+        
+    }
+    
+    func action(_ action: Action) {
+        switch action {
+            
+        }
+    }
+}
+
+// MARK: - Layout
+
+extension MomentTitleInputView {
+    
+    func configurationLayout() {
+        //
+    }
+}
---
+++ poporazzi/Utility/CodeBaseUIView+.swift
@@ -0,0 +1,46 @@
+//
+//  CodeBaseUIView.swift
+//  poporazzi
+//
+//  Created by 김민준 on 4/4/25.
+//
+
+import UIKit
+
+/// Code Base UIView 프로토콜을 편리하게 사용하기 위한 별칭
+typealias CodeBaseUIView = UIView & CodeBaseUIViewProtocol
+
+/// Code Base UIView 구현 시 필요한 필수 구현 프로토콜
+protocol CodeBaseUIViewProtocol: UIView {
+    
+    associatedtype Action
+    
+    /// 화면 세팅
+    func setup()
+    
+    /// 레이아웃 구성
+    func configurationLayout()
+    
+    /// 화면에서 실행 가능한 액션
+    func action(_ action: Action)
+}
+
+// MARK: - CodeBaseUIViewProtocol 기본 구현
+
+extension CodeBaseUIViewProtocol {
+    
+    func setup() {
+        self.backgroundColor = .white
+        configurationLayout()
+    }
+}
+
+// MARK: - UIView 기본 구현
+
+extension UIView {
+    
+    /// Code Base UIView 구현 시 필수 구현 생성자
+    convenience init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
