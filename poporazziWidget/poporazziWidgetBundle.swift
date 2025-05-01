b5d279633a2239894dadc547545c46120d345c47
thinkyside <eunlyuing@gmail.com>
2025-05-01 16:41:18 +0900

    [#43] poporazzi Widget Target 추가

---
+++ poporazziWidget/poporazziWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  poporazziWidgetBundle.swift
+//  poporazziWidget
+//
+//  Created by 김민준 on 5/1/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct poporazziWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        poporazziWidget()
+    }
+}
