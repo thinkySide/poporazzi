f2add70b3d5efe49d7a9b1fd277206383a1774ac
thinkyside <eunlyuing@gmail.com>
2025-05-02 13:46:56 +0900

    [#46] DIContainer 구현

---
+++ poporazzi/App/DIContainer.swift
@@ -0,0 +1,52 @@
+//
+//  DIContainer.swift
+//  poporazzi
+//
+//  Created by 김민준 on 5/2/25.
+//
+
+import Foundation
+
+final class DIContainer {
+    
+    static let shared = DIContainer()
+    private init() {}
+    
+    private var dependencies = [String: Any]()
+    
+    /// 객체를 등록합니다.
+    func register<T>(_ dependency: T) {
+        dependencies[key(from: dependency)] = dependency
+    }
+    
+    /// 객체를 꺼내옵니다.
+    func resolve<T>() -> T {
+        let key = key(from: T.self)
+        guard let dependency = dependencies[key] else {
+            fatalError("\(key): 등록되지 않은 객체")
+        }
+        return dependency as! T
+    }
+}
+
+// MARK: - Helper
+
+extension DIContainer {
+    
+    /// Dependency에 대한 Key를 생성합니다.
+    private func key<T>(from dependency: T) -> String {
+        String(describing: type(of: T.self))
+    }
+}
+
+// MARK: - PropertyWrapper
+
+@propertyWrapper
+final class Dependency<T> {
+    
+    let wrappedValue: T
+    
+    init() {
+        self.wrappedValue = DIContainer.shared.resolve()
+    }
+}
