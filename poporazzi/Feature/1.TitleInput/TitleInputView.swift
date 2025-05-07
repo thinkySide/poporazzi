83d26d2dbeec078fd54747d98b051b8e3ce6d0b4
thinkyside <eunlyuing@gmail.com>
2025-05-07 09:22:44 +0900

    [#64] TitleView TextField SubLabel 추가

--- poporazzi/Feature/1.TitleInput/TitleInputView.swift
+++ poporazzi/Feature/1.TitleInput/TitleInputView.swift
@@ -27,6 +27,15 @@ final class TitleInputView: CodeBaseUI {
     /// 제목 텍스트필드
     let titleTextField = LineTextField(size: 24, placeholder: "제주도 우정 여행, 성수동 데이트")
     
+    /// 제목 텍스트필드 보조 라벨
+    let titleTextFieldSubLabel: UILabel = {
+        let label = UILabel()
+        label.text = "앨범 제목은 언제든지 수정이 가능해요"
+        label.textColor = .subLabel
+        label.font = .setDovemayo(14)
+        return label
+    }()
+    
     /// 액션 버튼
     let actionButton = TextFieldActionButton(title: "기록 시작하기")
     
@@ -47,6 +56,26 @@ final class TitleInputView: CodeBaseUI {
     }
 }
 
+// MARK: - Action
+
+extension TitleInputView {
+    
+    enum Action {
+        case updateTitleTextFieldSubLabel
+    }
+    
+    func action(_ action: Action) {
+        switch action {
+        case .updateTitleTextFieldSubLabel:
+            if let text = titleTextField.textField.text, !text.isEmpty {
+                titleTextFieldSubLabel.isHidden = true
+            } else {
+                titleTextFieldSubLabel.isHidden = false
+            }
+        }
+    }
+}
+
 // MARK: - Layout
 
 extension TitleInputView {
@@ -58,6 +87,7 @@ extension TitleInputView {
                 flex.addItem().direction(.column).paddingHorizontal(20).define { flex in
                     flex.addItem(headerLabel).marginTop(64)
                     flex.addItem(titleTextField).marginTop(32)
+                    flex.addItem(titleTextFieldSubLabel).marginTop(12)
                 }
             }
         
